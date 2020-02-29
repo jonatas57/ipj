eab5ca7f7da2c1878668655b36a8fd5f2f10f12f
jonatas57 <jdjonatasduarte57@gmail.com>
2020-02-28 22:55:47 -0300

    pong

---
+++ Pong/Assets/Scripts/CreateNet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNet : MonoBehaviour {
+
+  public Transform dotPrefab;
+  public int numberDots;
+
+  // Start is called before the first frame update
+  void Start() {
+    float stepSize = 2 * Camera.main.orthographicSize / numberDots;
+    float y = -1f;
+    for (int i = 0;i < numberDots;i++, y += stepSize) {
+      Transform dotInstance = Instantiate<Transform>(dotPrefab);
+      dotInstance.name = "dot" + i;
+      dotInstance.parent = transform;
+      dotInstance.position = new Vector3(0, y, 0);
+    }
+  }
+}
---
+++ Pong/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+    
+    public Rigidbody2D rb;
+    public float speed;
+
+    public void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate() {
+        KeyCode upKey = KeyCode.W;
+        KeyCode downKey = KeyCode.S;
+        if (transform.localPosition.x > 0) {
+            upKey = KeyCode.UpArrow;
+            downKey = KeyCode.DownArrow;
+        }
+        
+        if (Input.GetKey(upKey)) {
+            rb.velocity = Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey)) {
+            rb.velocity = Vector2.down * speed;
+        }
+        else rb.velocity = Vector2.zero;
+    }
+}
